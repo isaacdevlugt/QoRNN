8958496f8bb4dfa533fa8d8755863c74c417263f
isaacdevlugt <ijsdevlugt@uwaterloo.ca>
2020-09-15 13:56:19 -0400

    Optimizing speed. Parameters currently not updating.

--- run_RNN.jl
+++ run_RNN.jl
@@ -1,14 +1,14 @@
 using Flux, DelimitedFiles
 using LinearAlgebra, Random
 using Distributions
-using Flux: onehot, onehotbatch, onecold, throttle, crossentropy, chunk, batchseq
+using Flux: onehot, onehotbatch, onecold, throttle, kldivergence, chunk, batchseq
 using Flux.Optimise: update!
 using Zygote
 
 Zygote.@nograd onehotbatch
 
 labels = 0:1
-batch_size = 100
+batch_size = 10
 
 train_path = "tfim1D_samples"
 psi_path = "tfim1D_psi"
@@ -16,36 +16,24 @@ psi_path = "tfim1D_psi"
 data = Int.(readdlm(train_path))
 psi = readdlm(psi_path)[:,1]
 
-train_data = data[1:5000, :]
-test_data = data[5001:end, :]
+train_data = data[1:10000, :]
+test_data = data[10001:end, :]
 
 N = size(train_data,2)
 
+train_data = [train_data[i,:] for i = 1:size(train_data, 1)]
+test_data = [test_data[i,:] for i = 1:size(test_data, 1)]
+
+train_data = map(ch -> onehotbatch(ch, labels), train_data)
+test_data = map(ch -> onehotbatch(ch, labels), test_data)
+
 m = Chain(
-    GRU(length(labels), 50),
-    GRU(50, 50),
-    GRU(50, length(labels)),
+    GRU(length(labels), 100),
+    GRU(100, length(labels)),
     softmax
 )
 m = f64(m)
-opt = Descent(0.01)
-
-function loss(batch)
-    l = 0 
-    
-    for i in 1:batch_size
-        Flux.reset!(m)
-        for n in 1:N-1
-        
-            l += crossentropy(m(batch[i,n]), batch[i,n+1])
-                
-        end
-    end
-        
-    l = l / batch_size
-
-    return l
-end
+opt = ADAM(0.01)
 
 function probability(v)
     if length(size(v)) == 1
@@ -73,6 +61,12 @@ function probability(v)
     end
     
     return prob
+end
+
+function loss(v)
+    # NLL
+    log_prob = log(probability(v))
+    return -sum(log_prob)
 end
 
 function generate_hilbert_space(;hot=false)                                               
@@ -105,7 +99,6 @@ function sample_model(num_samples)
         x = Bool.(reshape(rand.(d,1)[1], (2,)))
         y[s,1] = onecold(x, labels)[1]
         
-
         for i in 1:N-1
             x = onecold(x, labels)[1]
             x = onehot(x, labels)
@@ -132,29 +125,29 @@ function fidelity(space, target)
     return dot(target, sqrt.(probability(space)))
 end    
 
-train_data = map(ch -> onehot(ch, labels), train_data);
 train_data = [view(train_data, k:k+batch_size-1, :) for k in 1:batch_size:size(train_data,1)];
-
-test_data = map(ch -> onehot(ch, labels), test_data);
 test_data = [view(test_data, k:k+batch_size-1, :) for k in 1:batch_size:size(test_data,1)];
 
 epochs = 1:100
 num_batches = size(train_data,1) # needs to generalize
+println(num_batches)
 num_samples = 2000
 
 ps = Flux.params(m)
+space = generate_hilbert_space(hot=true) 
 
 for ep in epochs
-    println(ep)
     for b in 1:num_batches
         batch = train_data[b]
-        #testbatch = test_data[b]
+        testbatch = test_data[b]
         
-        gs = Flux.gradient(() -> loss(batch), ps)
+        gs = Flux.gradient(() -> loss_total(batch, batch), ps)
         #println(typeof(gs))
         #println(typeof(ps))
         update!(opt, ps, gs)
-        
+    
+        Flux.reset!(m)
+
         #for p in Flux.params(m)
         #    @show size(p)
         #    @show typeof(gs[p])
@@ -163,8 +156,8 @@ for ep in epochs
                 
     end
     
-    println("loss: ",loss(test_data[1]))
-    println("abs(mag): ", abs_magnetization(num_samples))
+    #println("loss: ",loss(test_data[1]))
+    #println("abs(mag): ", abs_magnetization(num_samples))
     println("fidelity: ", fidelity(space, psi))
     
 end
